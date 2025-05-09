1d8ac1cb20eee783b9fc6fc42e42d6a732bcd07d
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-05-09 11:06:41 +0200

    Jatekosok class készítése

--- legyenOnIsMilliomos/Program.cs
+++ legyenOnIsMilliomos/Program.cs
@@ -4,7 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Jatekosok player = new Jatekosok("Feng Min", 21, "Female", "Professional gamer");
+
 		}
 	}
 }
